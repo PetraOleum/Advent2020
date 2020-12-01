caa357c35beaba30ca662661641d748167198d0c
Petra Lamborn <petra@petras.space>
2020-12-01 18:26:44 +1300

    Second part of first day

--- 1b.R
+++ 1b.R
@@ -1,14 +1,18 @@
-# Target: find two numbers that sum to 2020, submit them multiplied together
+# Target: find *three* numbers that sum to 2020, submit them multiplied together
 
 # Get input
 input <- read.csv("input1a.csv", header=F)[[1]]
 
+# Add every pair of numbers together
+i2 <- outer(input, input, FUN = "+")
+
 # Find the value that would have to be present for them to add to 2020
-alt <- 2020 - input
+alt <- 2020 - i2
 
 # Returns the three numbers
 vals <- input[input %in% alt]
 vals
 
-# 719796
+# 144554112
 prod(vals)
+
